41c60d928a45508c986b3e9522b50b1f10a053d5
Diego-Panta <josepanta507@gmail.com>
2025-07-10 17:15:37 -0500

    Update views of login, register, home

--- app/src/main/java/com/episi/recyclens/LoginActivity.kt
+++ app/src/main/java/com/episi/recyclens/LoginActivity.kt
@@ -1,4 +1,22 @@
 package com.episi.recyclens
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        title = "Login"
+
+        // Ir a Register
+        val registerLink = findViewById<TextView>(R.id.register_link)
+        registerLink.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
